fae9372bac6f59e2a522e37f52896ef542ca6000
Gabriel Faundez <gnfaundez@uc.cl>
2017-10-02 10:31:58 -0300

    dev DbLogic

--- include/DbHandler.php
+++ include/DbHandler.php
@@ -7,11 +7,15 @@ class DbLogic
 
   function __construct(conn)
   {
-    private $db = conn;
+    private $db = conn.connect();
   }
 
-  function find(type,conditions){
-    $query = ; 
+  function find(type,labels,conditions){
+    $query = $this->db->prepare("");
+    for($i = 0; $i < sizeof($conditions);$i++){
+      $quey -> bindParam($i+1,$conditions[$i]);
+    }
+    $query->execute();
   }
 }
 
