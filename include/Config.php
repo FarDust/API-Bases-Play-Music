c594a7f43b0ef809fa3d551157db09c282bb2d2b
Gabriel Faundez <gnfaundez@uc.cl>
2017-10-02 09:34:00 -0300

    API connection

--- include/Config.php
+++ include/Config.php
@@ -4,6 +4,7 @@ define('DB_USERNAME', '');
 define('DB_PASSWORD', '');
 define('DB_HOST', '');
 define('DB_NAME', '');
+define('DB_PORT', '')
 
 define('API_KEY','3d524a53c110e4c22463b10ed32cef9d');
 
--- include/DbConnect.php
+++ include/DbConnect.php
@@ -1,2 +1,35 @@
 <?php
+
+class Connection {
+
+  private $link;
+
+  function __construct(){
+    
+  }
+
+  function connect(){
+    include_once dirname(__FILE__).'./Config.php'
+
+    try {
+      $this->link = new PDO('psql:host='.DB_HOST.
+                            ';port='.DB_PORT.
+                            ';dbname='.DB_NAME.
+                            ';user='.DB_USERNAME.
+                            ';password='.DB_PASSWORD);
+      $this->link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $this->link->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+      return $this->link;
+
+    } catch (PDOException $e) {
+        if (defined('ENVIRONMENT') && ENVIRONMENT == 'dev'){
+          echo $e->getMessage();
+        }
+      exit;
+    }
+
+  }
+}
+
 ?>
