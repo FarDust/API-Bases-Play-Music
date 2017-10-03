60f0ae3e413efa9fe2c05c1db3c674face18a05c
Gabriel Faundez <gnfaundez@uc.cl>
2017-10-02 22:45:27 -0300

    php fix

--- v1/index.php
+++ v1/index.php
@@ -5,28 +5,28 @@ include("../include/DbConnect.php");
 include_once("../include/DbHandler.php");
 
 if (isset($_POST['rule']) && $_POST['rule'] == 'find') {
-  $responce;
+  $response;
   $db = new Connection();
   $logic = new DbLogic($db);
   unset($_POST['rule']);
   if (isset($_POST['option']) && $_POST['option'] == 'banda'){
     $sql = 'SELECT * FROM Banda WHERE Banda.nombre = ?';
     unset($_POST['option']);
-    $responce = $logic->bind($sql,$_POST);
+    $response = $logic->bind($sql,$_POST);
     header(http_response_code(200));
   }elseif (isset($_POST['option']) && $_POST['option'] == 'artista') {
     $sql = 'SELECT * FROM Artista WHERE Banda.nombre = ?';
     unset($_POST['option']);
-    $responce = $logic->bind($sql,$_POST);
+    $response = $logic->bind($sql,$_POST);
     header(http_response_code(200));
   } else {
     header(http_response_code(400));
-    $responce = "{'error' : 400}";
+    $response = "{'error' : 400}";
   }
-  echo json_encode($responce);
+  echo json_encode($response);
 }else{
   header(http_response_code(405));
-  $responce = "{'error' : 400}";
-  echo json_encode($responce);
+  $response = "{'error' : 400}";
+  echo json_encode($response);
 }
  ?>
