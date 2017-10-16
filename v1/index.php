9e6c7737545e23ea8458a5494530e7f3e6138ee4
Gabriel Faundez <gnfaundez@uc.cl>
2017-10-16 14:21:27 -0300

    update

--- v1/index.php
+++ v1/index.php
@@ -29,6 +29,7 @@ if (isset($_POST['rule']) && $_POST['rule'] == 'find') {
     $response = $logic9->bind($sql,$_POST);
     header(http_response_code(200));
   }elseif (isset($_POST['option']) && $_POST['option'] == 'artista') {
+    unset($_POST['option']);
     $sql =
       'SELECT Artista.nombre, Email.email
       FROM Banda, Miembro, Artista, HasEmail, Email
@@ -38,9 +39,23 @@ if (isset($_POST['rule']) && $_POST['rule'] == 'find') {
       AND Artista.id = HasEmail.id
       AND HasEmail.email = Email.email
       AND Miembro.fecha_abandono = null';
-    unset($_POST['option']);
     $response = $logic9->bind($sql,$_POST);
     $fullresponse['a_members'] = $response;
+    $sql =
+      'SELECT Artista.nombre
+      FROM Banda,Disco, BandaAutorOf, Miembro, Artista
+      WHERE Banda.id = BandaAutorOf.idd
+      AND Disco.id = BandaAutorOf.idd
+      AND Banda.id = Miembro.idb
+      AND Artista.id = Miembro.ida
+      AND Artista.nombre = ?
+      UNION SELECT Artista.nombre
+      FROM Artista, Disco, ArtistaAutorOf
+      WHERE Artista.id = ArtistaAutorOf.idd
+      AND Disco.id = ArtistaAutorOf.idd
+      AND Artista.nombre = ?';
+    $response = $logic9->bind($sql,$_POST);
+    $fullresponse['discs'] = $response
     header(http_response_code(200));
   } else {
     header(http_response_code(400));
