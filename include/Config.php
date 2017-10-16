2e9fb98aaff1c6d0ab2f23ce9cc1765575defc91
Gabriel Faundez <gnfaundez@uc.cl>
2017-10-16 14:58:37 -0300

    fatal error

--- include/Config.php
+++ include/Config.php
@@ -6,9 +6,9 @@ define('DB_HOST', 'localhost');
 define('DB_NAME', 'grupo9');
 define('DB_PORT', '5432');
 
-define('DB_USERNAME', 'grupo28');
-define('DB_PASSWORD', 'grupo28');
-define('DB_NAME', 'grupo28');
+define('DB_USERNAME1', 'grupo28');
+define('DB_PASSWORD1', 'grupo28');
+define('DB_NAME1', 'grupo28');
 
 define('API_KEY','3d524a53c110e4c22463b10ed32cef9d');
 
