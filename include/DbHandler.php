cb11334264a61925d81fcbb2ad310bacafd1d728
Gabriel Faundez <gnfaundez@uc.cl>
2017-10-16 19:33:01 -0300

    SQL update

--- include/DbHandler.php
+++ include/DbHandler.php
@@ -12,10 +12,10 @@ class DbLogic
 
   function bind($sql,$conditions){
     $query = $this->db->prepare($sql);
-    if (isset[$conditions["rule"]] && $conditions["rule"] == "find"){
+    if (isset($conditions["rule"]) && $conditions["rule"] == "find"){
       $query->bindParam(1,$conditions['nombre']);
     }
-    
+
     return $query->execute();
   }
 }
