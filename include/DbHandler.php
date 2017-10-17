8f1cedb366f62ff3a3cf24376e2608bacff8d7a2
Gabriel Faundez <gnfaundez@uc.cl>
2017-10-16 21:16:00 -0300

    SQL update

--- include/DbHandler.php
+++ include/DbHandler.php
@@ -14,7 +14,8 @@ class DbLogic
     $query = $this->db->prepare($sql);
     if (isset($conditions["rule"]) && $conditions["rule"] == "find"){
       $query->bindParam(1,$conditions['nombre'],PDO::PARAM_STR);
-      return $query->execute()-> fetchAll();
+      $query->execute();
+      return $query-> fetchAll();
     }
 
   }
